720bf8eafa35f7fc789688d8b817b7a65874ced4
Ifi24 <esperanza.ifesinachi.egbera@estudinatat.upc.edu>
2026-03-10 10:42:42 +0100

    Add project files.

---
+++ FlightLib/FlightPlan.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightLib
+{
+    public class FlightPlan
+    {
+        // Atributos
+
+        string id; // identificador
+        Position currentPosition; // posicion actual
+        Position finalPosition; // posicion final
+        double velocidad;
+
+        // Constructures
+        public FlightPlan(string id, double cpx, double cpy, double fpx, double fpy, double velocidad)
+        {
+            this.id = id;
+            this.currentPosition = new Position(cpx, cpy);
+            this.finalPosition = new Position(fpx, fpy);
+            this.velocidad = velocidad;
+        }
+
+        // Metodos
+
+        public void SetVelocidad(double velocidad)
+        // setter del atributo velocidad
+        { this.velocidad = velocidad; }
+
+        public void Mover(double tiempo)
+        // Mueve el vuelo a la posición correspondiente a viajar durante el tiempo que se recibe como parámetro
+        {
+            //Calculamos la distancia recorrida en el tiempo dado
+            double distancia = tiempo * this.velocidad / 60;
+
+            //Calculamos las razones trigonométricas
+            double hipotenusa = Math.Sqrt((finalPosition.GetX() - currentPosition.GetX()) * (finalPosition.GetX() - currentPosition.GetX()) + (finalPosition.GetY() - currentPosition.GetY()) * (finalPosition.GetY() - currentPosition.GetY()));
+            double coseno = (finalPosition.GetX() - currentPosition.GetX()) / hipotenusa;
+            double seno = (finalPosition.GetY() - currentPosition.GetY()) / hipotenusa;
+
+            //Caculamos la nueva posición del vuelo
+            double x = currentPosition.GetX() + distancia * coseno;
+            double y = currentPosition.GetY() + distancia * seno;
+
+            // Cambiamos el nombre porque la vamos a usar para saber si estamos todavía en el vuelo o si hemos llegado al final de este por lo que nos pararíamos 
+            // Hay que poner Position porque nextPosition no está declarada, antes no la poníamos porque la hemos declarado antes
+            Position nextPosition = new Position(x, y);
+
+            if (currentPosition.Distancia(nextPosition) < hipotenusa)
+                currentPosition = nextPosition;
+            else
+                currentPosition = finalPosition;
+        }
+
+
+        // Método para saber si un vuelo ha llegado a su destino o no
+        public bool VueloDestino()
+        {
+            bool destino = false;
+            if (currentPosition == finalPosition)
+                destino = true;
+            return destino;
+        }
+
+
+        // Método que detecta el conflicto según la distancia de seguridad proporcionada
+        public bool ConflictoDistancia(FlightPlan b, double distanciaSeguridad)
+        {
+            bool conflicto = false;
+            if (this.currentPosition.Distancia(b.currentPosition) < distanciaSeguridad)
+                conflicto = true;
+            return conflicto;
+        }
+
+
+        public void EscribeConsola()
+        // escribe en consola los datos del plan de vuelo
+        {
+            Console.WriteLine("******************************");
+            Console.WriteLine("Datos del vuelo: ");
+            Console.WriteLine("Identificador: {0}", id);
+            // Hemos hecho que la velocidad y la posición sean floats con dos decimales
+            Console.WriteLine("Velocidad: {0:F2}", velocidad);
+            Console.WriteLine("Posición actual: ({0:F2},{1:F2})", currentPosition.GetX(), currentPosition.GetY());
+            // Hacemos que en el caso de que nuestro método nos devuelva true, escriba el mensaje siguiente: "El vuelo ha llegado a su destino"
+            if (this.VueloDestino()) 
+                Console.WriteLine("El vuelo ha llegado a su destino");
+            // En el caso de que nuestro método nos devuelva false, hacemos que escriba el siguiente mensaje: "El vuelo todavía no ha llegado a su destino" 
+            else
+                Console.WriteLine("El vuelo todavía no ha llegado a su destino");
+            Console.WriteLine("******************************");
+        }
+    }
+}
---
+++ SimulatorConsole/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlightLib;
+
+namespace SimulatorConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            FlightPlanList lista = new FlightPlanList();
+            try 
+            {
+                Console.WriteLine("Escribe el identificador");
+                //   string nombre = Console.ReadLine();
+                string identificador = Console.ReadLine(); ;
+
+                Console.WriteLine("Escribe la velocidad");
+                double velocidad = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Escribe las coordenadas de la posición inicial, separadas por un blanco");
+                string linea = Console.ReadLine();
+                string[] trozos = linea.Split(' ');
+                double ix = Convert.ToDouble(trozos[0]);
+                double iy = Convert.ToDouble(trozos[1]);
+
+                Console.WriteLine("Escribe las coordenadas de la posición final, separadas por un blanco");
+                linea = Console.ReadLine();
+                trozos = linea.Split(' ');
+                double fx = Convert.ToDouble(trozos[0]);
+                double fy = Convert.ToDouble(trozos[1]);
+
+                FlightPlan plan_a = new FlightPlan(identificador, ix, iy, fx, fy, velocidad);
+
+                Console.WriteLine("Escribe el identificador");
+                //   string nombre = Console.ReadLine();
+                identificador = Console.ReadLine(); ;
+
+                Console.WriteLine("Escribe la velocidad");
+                velocidad = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Escribe las coordenadas de la posición inicial, separadas por un blanco");
+                linea = Console.ReadLine();
+                trozos = linea.Split(' ');
+                ix = Convert.ToDouble(trozos[0]);
+                iy = Convert.ToDouble(trozos[1]);
+
+                Console.WriteLine("Escribe las coordenadas de la posición final, separadas por un blanco");
+                linea = Console.ReadLine();
+                trozos = linea.Split(' ');
+                fx = Convert.ToDouble(trozos[0]);
+                fy = Convert.ToDouble(trozos[1]);
+
+                FlightPlan plan_b = new FlightPlan(identificador, ix, iy, fx, fy, velocidad);
+
+                lista.AddFlightPlan(plan_a);
+                lista.AddFlightPlan(plan_b);
+
+                double distanciaSeguridad = 10;
+
+                int i = 0; // Empezamos dándole a i un valor de inicio (0), que luego usaremos para saber si nos mantenemos en el bucle o lo terminamos
+                int ciclos = 50; // Determinamos el número de ciclos que queremos
+
+                while (i < ciclos) // Hasta que i no llegue al número de ciclos que queremos y hemos establecido, seguirá realizando el bucle
+                {
+                    lista.Mover(10);
+                    lista.EscribeConsola();
+                    if (plan_a.ConflictoDistancia(plan_b, distanciaSeguridad))
+                        Console.WriteLine("Hay un conflicto con la distancia de seguridad.");
+                    i++; // Mientras la i sea menor que el número de ciclos se le sumará 1 para pasar al siguiente ciclo
+                }
+                plan_a.EscribeConsola();
+            }
+            catch (FormatException) // Si hemos instroducido los datos de manera incorrecta, nos devolverá el mensaje: "Introduce los datos correctamente, por favor."
+            {
+                Console.WriteLine("Introduce los datos correctamente, por favor.");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
